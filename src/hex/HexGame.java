8c2ee61f4c1451ccd0ba27ad6fe5b9f0c5b7515d
Trung Nguyen <tnguyen9@macalester.edu>
2017-05-02 02:24:30 -0500

    Initial commit

---
+++ src/hex/HexGame.java
@@ -0,0 +1,14 @@
+package hex;
+
+import comp124graphics.CanvasWindow;
+
+public class HexGame extends CanvasWindow {
+
+    /* Private constants */
+    private static final int CANVAS_WIDTH = 800;
+    private static final int CANVAS_HEIGHT = 1000;
+
+    public HexGame() {
+        super("Hex Game", CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
---
+++ src/hex/Hexagon.java
@@ -0,0 +1,16 @@
+package hex;
+
+import comp124graphics.GraphicsObject;
+
+/**
+ * Created by Trung Nguyen on 5/2/2017.
+ *
+ */
+public class Hexagon extends GraphicsObject {
+
+    private double x;
+    private double y;
+
+    public Hexagon() {}
+
+}
