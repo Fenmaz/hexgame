a4abf52022c7faa173380748e5fe8484877d94e5
Trung Nguyen <tnguyen9@macalester.edu>
2017-05-02 22:54:41 -0500

    Comments and documentation

--- src/hex/GameBoard.java
+++ src/hex/GameBoard.java
@@ -3,19 +3,21 @@ package hex;
 import comp124graphics.GraphicsGroup;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created by Trung Nguyen on 5/2/2017.
  *
+ * The game board.
  */
 public class GameBoard extends GraphicsGroup {
 
 
     private ArrayList<Hexagon> allHexes;
 
-    public GameBoard(int numHexOnEdge) {
+    /**
+     * Draw the game board.
+     */
+    GameBoard(int numHexOnEdge) {
         for (int i = 0; i < numHexOnEdge; i++) {
             for (int j = 0; j < numHexOnEdge; j++) {
                 Hexagon hex = new Hexagon((i - j) * HexGame.HEX_HEIGHT, (i + j) * HexGame.HEX_RADIUS * 2);
@@ -25,12 +27,18 @@ public class GameBoard extends GraphicsGroup {
         }
     }
 
-    public boolean firstPlayerWin() {
+    /**
+     * Check if the first player wins.
+     */
+    boolean firstPlayerWin() {
         // TODO: Check if first player wins by a DFS on the hexagons starting from the SW region
         return false;
     }
 
-    public boolean secondPlayerWin() {
+    /**
+     * Check if the second player wins.
+     */
+    boolean secondPlayerWin() {
         // TODO: Check if second player wins by a DFS on the hexagons starting from the NW region
         return false;
     }
--- src/hex/HexGame.java
+++ src/hex/HexGame.java
@@ -1,11 +1,15 @@
 package hex;
 
 import comp124graphics.CanvasWindow;
+import comp124graphics.GraphicsObject;
 import comp124graphics.GraphicsText;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+/**
+ * Primary game setup and logic.
+ */
 public class HexGame extends CanvasWindow implements MouseListener {
 
     /* Private constants */
@@ -18,8 +22,8 @@ public class HexGame extends CanvasWindow implements MouseListener {
     private static final int FIRST_PLAYER = 1;
     private static final int SECOND_PLAYER = 2;
 
-    protected static final double HEX_RADIUS = 10;
-    protected static final double HEX_HEIGHT = HexGame.HEX_RADIUS * Math.sqrt(3) / 2;
+    static final double HEX_RADIUS = 10;
+    static final double HEX_HEIGHT = HexGame.HEX_RADIUS * Math.sqrt(3) / 2;
 
 
     /* Private variables */
@@ -28,6 +32,9 @@ public class HexGame extends CanvasWindow implements MouseListener {
     private int turn;
 
 
+    /**
+     * Initialize the game board and mouse listeners. Player 1 moves first.
+     */
     public HexGame() {
 
         super("Hex Game", CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -39,26 +46,34 @@ public class HexGame extends CanvasWindow implements MouseListener {
         addMouseListener(this);
     }
 
+    /**
+     * When a hexagon is clicked, change the hex, check for winner and change turns.
+     */
     @Override
     public void mouseClicked(MouseEvent e) {
-        Hexagon hex = (Hexagon) getElementAt(e.getX(), e.getY());
-        // TODO: associate the hex with the player at that turn
+        GraphicsObject hex = getElementAt(e.getX(), e.getY());
+        if (hex != null) {
+            // TODO: associate the hex with the player at that turn
 
-        // Change turns
-        if (turn == FIRST_PLAYER) turn = SECOND_PLAYER;
-        else turn = FIRST_PLAYER;
+            // Check if either player wins
+            if (board.firstPlayerWin()) announceWinner(FIRST_PLAYER);
+            if (board.secondPlayerWin()) announceWinner(SECOND_PLAYER);
 
-        // Check if either player wins
-        if (board.firstPlayerWin()) announceWinner(FIRST_PLAYER);
-        if (board.secondPlayerWin()) announceWinner(SECOND_PLAYER);
-
+            // Change turns
+            if (turn == FIRST_PLAYER) turn = SECOND_PLAYER;
+            else turn = FIRST_PLAYER;
+        }
     }
 
+    /**
+     * Announce the winner with a text in the middle of the screen
+     */
     private void announceWinner(int winner) {
         GraphicsText announcement = new GraphicsText("Player " + winner + " wins!", 0, 0);
         announcement.move((CANVAS_WIDTH - announcement.getWidth()) / 2, (CANVAS_HEIGHT - announcement.getHeight()) / 2);
     }
 
+    // Unused mouse listener methods
     public void mouseEntered(MouseEvent e) {}
 
     public void mouseExited(MouseEvent e) {}
