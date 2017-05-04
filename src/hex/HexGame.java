15d8f154008e2327229ba7ebc2b8e846ccd5adf1
Trung Nguyen <tnguyen9@macalester.edu>
2017-05-03 23:10:45 -0500

    Resize and move the winner announcement

--- src/hex/HexGame.java
+++ src/hex/HexGame.java
@@ -4,6 +4,7 @@ import comp124graphics.CanvasWindow;
 import comp124graphics.GraphicsObject;
 import comp124graphics.GraphicsText;
 
+import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -14,9 +15,9 @@ class HexGame extends CanvasWindow implements MouseListener {
 
     /* Private constants */
     private static final int CANVAS_WIDTH = 1000;
-    private static final int CANVAS_HEIGHT = 600;
+    private static final int CANVAS_HEIGHT = 800;
 
-    private static final int NUM_HEX_ON_EDGE = 1;
+    private static final int NUM_HEX_ON_EDGE = 11;
 
     static final int FREE_HEX = 0;
     static final int FIRST_PLAYER = 1;
@@ -25,13 +26,15 @@ class HexGame extends CanvasWindow implements MouseListener {
     static final double HEX_RADIUS = 30;
     static final double HEX_HEIGHT = (HEX_RADIUS * Math.sqrt(3) / 2);
 
+    private static final int ANNOUNCEMENT_FONT_SIZE = 40;
+
 
     /* Private variables */
     private GameBoard board;
 
     private int turn;
-
     private boolean gameOver;
+    private GraphicsText announcement;
 
 
     /**
@@ -47,6 +50,11 @@ class HexGame extends CanvasWindow implements MouseListener {
         turn = FIRST_PLAYER;
         gameOver = false;
 
+        announcement = new GraphicsText("Player 0 wins!", 0, 0);
+        announcement.setFont(new Font("SanSerif", Font.BOLD, ANNOUNCEMENT_FONT_SIZE));
+        // announcement.setColor(Color.BLUE);
+        announcement.move((CANVAS_WIDTH - announcement.getWidth()) / 2, (CANVAS_HEIGHT - announcement.getHeight()));
+
         addMouseListener(this);
     }
 
@@ -55,31 +63,41 @@ class HexGame extends CanvasWindow implements MouseListener {
      */
     @Override
     public void mouseClicked(MouseEvent e) {
-        GraphicsObject hex = board.getElementAt(e.getX(), e.getY());
+        if (!gameOver){
+            GraphicsObject hex = board.getElementAt(e.getX(), e.getY());
 
-        if (hex instanceof Hexagon && ((Hexagon) hex).getPlayer() == FREE_HEX) {
-            System.out.print(e.getX() + ", " + e.getY() + "\n");
-            System.out.print("Marking hex" + hex.getPosition() + "\n");
-            ((Hexagon) hex).mark(turn);
+            if (hex instanceof Hexagon && ((Hexagon) hex).getPlayer() == FREE_HEX) {
+                ((Hexagon) hex).mark(turn);
 
-            // Check if either player wins
-            if (board.firstPlayerWin()) announceWinner(FIRST_PLAYER);
-            if (board.secondPlayerWin()) announceWinner(SECOND_PLAYER);
+                // Check if either player wins
+                if (board.firstPlayerWin()) announceWinner(FIRST_PLAYER);
+                if (board.secondPlayerWin()) announceWinner(SECOND_PLAYER);
 
-            // Change turns
-            if (turn == FIRST_PLAYER) turn = SECOND_PLAYER;
-            else turn = FIRST_PLAYER;
+                // Change turns
+                if (turn == FIRST_PLAYER) turn = SECOND_PLAYER;
+                else turn = FIRST_PLAYER;
+            }
         }
+        else {
+            remove(announcement);
+            remove(board);
+
+            board = new GameBoard(NUM_HEX_ON_EDGE);
+            add(board, (CANVAS_WIDTH - board.getWidth()) / 2, (CANVAS_HEIGHT - board.getHeight()) / 2);
+
+            turn = FIRST_PLAYER;
+            gameOver = false;
+        }
+
     }
 
     /**
      * Announce the winner with a text in the middle of the screen
      */
     private void announceWinner(int winner) {
-        System.out.print("Winner is decided");
-        GraphicsText announcement = new GraphicsText("Player " + winner + " wins!", 0, 0);
-        announcement.move((CANVAS_WIDTH - announcement.getWidth()) / 2, (CANVAS_HEIGHT - announcement.getHeight()) / 2);
+        announcement.setText("Player " + winner + " wins!");
         add(announcement);
+        gameOver = true;
     }
 
     // Unused mouse listener methods
